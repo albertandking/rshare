c9475cd412b6e64497d4411608c5a44ca6850eec
jindaxiang <jindaxiang@163.com>
2024-03-03 03:11:23 +0800

    format py-rshare

--- py-rshare/rshare/__init__.py
+++ py-rshare/rshare/__init__.py
@@ -1,2 +1,7 @@
-from .rshare import *
-from .fetch import *
+from rshare import *
+from rshare.fetch import *
+
+from rshare.utils._rshare_version import get_rshare_version
+
+__version__: str = get_rshare_version()
+del get_rshare_version
